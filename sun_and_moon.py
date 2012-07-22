ec8e10fa7889d1ac6020f31073cc7d8f5e387dfb
Mark Sundstrom <mark@mhsundstrom.com>
2012-07-22 11:42:09 -0400

    sun_and_moon now prints utf8
    This makes GeekTool happy. Otherwise it was getting UnicodeEncode
    errors.

--- sun_and_moon.py
+++ sun_and_moon.py
@@ -9,12 +9,14 @@
 
     I use GeekTool to display the output of this on the desktop background:
         http://projects.tynsoe.org/en/geektool/
-    I'm sure this is a similar tool available on Windows or Linux.
-
-    Requires the PyEphem package, available at:
-        http://rhodesmill.org/pyephem/
+    A similar tool for Windows is called Rainmeter:
+        http://rainmeter.net/
 
     Using Python 2.7
+
+    The utf-8 encoding trick below seemed to be the easiest way to
+    get the output to show up in GeekTool without generating a
+    UnicodeEncodeError
 """
 from __future__ import print_function
 import ephem
@@ -25,6 +27,11 @@ observer = ephem.city(defaults.CITY)
 
 
 def calculate(symbol, body):
+    """
+        Calculate the appropriate rising and setting time. We'll
+        calculate tomorrow's time if the body is not currently
+        above the horizon.
+    """
     if body.alt > 0:
         az, alt = [degrees(float(i)) for i in (body.az, body.alt)]
         current = u'Azi {:3.0f}\xb0 Alt {:.0f}\xb0'.format(az, alt)
@@ -37,10 +44,12 @@ def calculate(symbol, body):
     r = fmt.format(u'Rise', rising, degrees(float(body.az)))
     setting = ephem.localtime(observer.next_setting(body))
     s = fmt.format(u'Set', setting, degrees(float(body.az)))
-    print(u'{}{} {}\u2605{}'.format(symbol, body.name, r, s))
+    yield u'{}{} {}\u2605{}'.format(symbol, body.name, r, s)
     if current:
-        print(u'{} {}'.format(symbol, current))
+        yield u'{} {}'.format(symbol, current)
 
-
-calculate(u'\u263c', ephem.Sun(observer))
-calculate(u'\u263d', ephem.Moon(observer))
+source = (u'\u263c', ephem.Sun(observer)), (u'\u263d', ephem.Moon(observer))
+for symbol, body in source:
+    for line in calculate(symbol, body):
+        s = line.encode('utf8')
+        print(s)
