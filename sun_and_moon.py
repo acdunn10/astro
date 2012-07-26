30185c41c8660b0fffc73dfd530d9a54e7d016fd
Mark Sundstrom <mark@mhsundstrom.com>
2012-07-26 08:21:08 -0400

    unicode and display tweaks for sun_and_moon

--- sun_and_moon.py
+++ sun_and_moon.py
@@ -14,22 +14,35 @@
 
     Using Python 2.7
 
-    The utf-8 encoding trick below seemed to be the easiest way to
-    get the output to show up in GeekTool without generating a
-    UnicodeEncodeError
+    GeekTool gets a UnicodeEncodeError unless I use line.encode('utf8')
 """
 from __future__ import print_function
+from __future__ import unicode_literals
 import ephem
 from math import degrees
 import itertools
 import defaults
 
-SUN_SYMBOL = u'\u263c'
-MOON_SYMBOL = u'\u263d'
+SUN_SYMBOL = '\u263c'
+MOON_SYMBOL = '\u263d'
+METERS_PER_MILE = 1609.344
 
 observer = ephem.city(defaults.CITY)
 sun = ephem.Sun(observer)
 moon = ephem.Moon(observer)
+
+SOURCE = ((SUN_SYMBOL, sun), (MOON_SYMBOL, moon))
+
+
+def sky_position():
+    current = []
+    for symbol, body in SOURCE:
+        if body.alt > 0:
+            az, alt = [degrees(float(i)) for i in (body.az, body.alt)]
+            current.append('{} Azi {:3.0f}\xb0 Alt {:.0f}\xb0'.format(
+                symbol, az, alt))
+    yield '   '.join(current)
+
 
 def calculate(symbol, body):
     """
@@ -39,30 +52,23 @@ def calculate(symbol, body):
     """
     if body.alt > 0:
         az, alt = [degrees(float(i)) for i in (body.az, body.alt)]
-        current = u'Azi {:3.0f}\xb0 Alt {:.0f}\xb0'.format(az, alt)
         rising_method = observer.previous_rising
     else:
-        current = None
         rising_method = observer.next_rising
-    fmt = u'{} {:%I:%M %p %a} Azi {:.0f}\xb0'
+    fmt = '{} {:%I:%M %p %a} Azi {:.0f}\xb0'
     rising = ephem.localtime(rising_method(body))
-    r = fmt.format(u'Rise', rising, degrees(float(body.az)))
+    r = fmt.format('Rise', rising, degrees(float(body.az)))
     setting = ephem.localtime(observer.next_setting(body))
-    s = fmt.format(u'Set', setting, degrees(float(body.az)))
-    yield u'{}{} {}\u2605{}'.format(symbol, body.name, r, s)
-    if current:
-        yield u'{} {}'.format(symbol, current)
+    s = fmt.format('Set', setting, degrees(float(body.az)))
+    yield '{}{}   {}{}'.format(symbol, r, symbol, s)
 
 
 def sun_and_moon():
     "Rise, set and current position of the Sun and Moon"
-    source = (
-        (SUN_SYMBOL, sun),
-        (MOON_SYMBOL, moon)
-        )
-    for symbol, body in source:
+    for symbol, body in SOURCE:
         for line in calculate(symbol, body):
             yield line
+
 
 def moon_info():
     "A bit more useful information about the Moon"
@@ -70,27 +76,19 @@ def moon_info():
     previous_new = ephem.previous_new_moon(now)
     moon_age = 24 * (now - previous_new)
     if moon_age <= 72:
-        yield u"{} Young Moon: {:.1f} hours".format(MOON_SYMBOL, moon_age)
+        yield "{} Young Moon: {:.1f} hours".format(MOON_SYMBOL, moon_age)
     else:
         next_new = ephem.next_new_moon(now)
         moon_age = 24 * (next_new - now)
         if moon_age <= 72:
-            yield u"{} Old Moon {:.1f} hours".format(MOON_SYMBOL, moon_age)
+            yield "{} Old Moon {:.1f} hours".format(MOON_SYMBOL, moon_age)
     moon.compute(now)
-    moon_distance_miles = ephem.meters_per_au * moon.earth_distance / 1609.344
-    yield u"{}Phase {:.2f}%, {:,.1f} miles".format(
+    moon_distance_miles = ephem.meters_per_au *\
+        moon.earth_distance / METERS_PER_MILE
+    yield "{}Phase {:.2f}%, {:,.1f} miles".format(
         MOON_SYMBOL, moon.phase, moon_distance_miles)
-    events = [
-        ("First Quarter", ephem.next_first_quarter_moon(now)),
-        ("Full Moon", ephem.next_full_moon(now)),
-        ("Last Quarter", ephem.next_last_quarter_moon(now)),
-        ("New Moon", ephem.next_new_moon(now))
-        ]
-    events.sort(key=lambda x:x[1])
-    yield u"{}{} {}".format(MOON_SYMBOL, *events[0])
 
 
 if __name__ == '__main__':
-    for line in itertools.chain(sun_and_moon(), moon_info()):
-        s = line.encode('utf8')
-        print(s)
+    for line in itertools.chain(sky_position(), sun_and_moon(), moon_info()):
+        print(line.encode('utf8'))
