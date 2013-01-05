06962fa42c0bd50745abed437c8190a42033b3e6
Mark Sundstrom <mark@mhsundstrom.com>
2013-01-05 06:13:41 -0500

    SunEvents checkpoint

--- astro/sun.py
+++ astro/sun.py
@@ -3,75 +3,84 @@
 """
 import os
 import ephem
-from math import degrees
 import itertools
-from . import CITY, MILES_PER_AU
+import collections
+import operator
+from sorted_collection import SortedCollection
+import json
+from . import CITY, astro_path
+
+class SunEvent(collections.namedtuple('SunEvent', 'date event')):
+    def __str__(self):
+        #return '{0.date} {0.event}'.format(self)
+        return '{:%Y-%m-%d %H:%M:%S} {}'.format(ephem.localtime(self.date), self.event)
+
+Horizon = collections.namedtuple('Horizon', 'name degrees')
+
+twilights = [
+    Horizon('Civil Twilight', ephem.degrees('-6')),
+    Horizon('Nautical Twilight', ephem.degrees('-12')),
+    Horizon('Astronomical Twilight', ephem.degrees('-18'))
+    ]
+riseset = Horizon('Rise or Set', ephem.degrees('-0:34'))
+
+def make_all_sun_events(sun, observer, date):
+    noon = int(date)
+    dates = [ephem.date(i) for i in (noon-1, noon, noon+1)]
+    events = SortedCollection(key=operator.attrgetter('date'))
+    observer.pressure = 0
+    for date in dates:
+        observer.horizon = riseset.degrees
+        for func in (observer.previous_rising, observer.next_rising):
+            events.insert(SunEvent(func(sun, start=date), 'Rise'))
+        for func in (observer.previous_setting, observer.next_setting):
+            events.insert(SunEvent(func(sun, start=date), 'Set'))
+        for twi in twilights:
+            observer.horizon = twi.degrees
+            for func in (observer.previous_rising, observer.next_rising):
+                events.insert(SunEvent(func(sun, start=date), 'Morning {0.name}'.format(twi)))
+            for func in (observer.previous_setting, observer.next_setting):
+                events.insert(SunEvent(func(sun, start=date), 'Evening {0.name}'.format(twi)))
+    return events
+
+def make_sun_events(sun, observer, date):
+    events = SortedCollection(key=operator.attrgetter('date'))
+    observer.pressure = 0
+    observer.horizon = riseset.degrees
+    for func in (observer.previous_rising, observer.next_rising):
+        events.insert(SunEvent(func(sun, start=date), 'Rise'))
+    for func in (observer.previous_setting, observer.next_setting):
+        events.insert(SunEvent(func(sun, start=date), 'Set'))
+    for twi in twilights:
+        observer.horizon = twi.degrees
+        for func in (observer.previous_rising, observer.next_rising):
+            events.insert(SunEvent(func(sun, start=date), 'Morning {0.name}'.format(twi)))
+        for func in (observer.previous_setting, observer.next_setting):
+            events.insert(SunEvent(func(sun, start=date), 'Evening {0.name}'.format(twi)))
+    return events
 
 
-observer = ephem.city(CITY)
-sun = ephem.Sun(observer)  # ☼
-
-if sun.alt > 0:
-    up_or_down = '⬆' if sun.az <= ephem.degrees('180') else '⬇'
-    print('☼ {}{} {} ⇔ '.format(sun.alt, up_or_down, sun.az))
-elif sun.alt >= ephem.degrees('-6'):
-    print('Civil twilight', sun.alt)
-elif sun.alt >= ephem.degrees('-12'):
-    print('Nautical twilight', sun.alt)
-elif sun.alt >= ephem.degrees('-18'):
-    print('Astronomical twilight', sun.alt)
-else:
-    print("It's dark out there", sun.az)
 
 
+if __name__ == '__main__':
+    observer = ephem.city(CITY)
+    sun = ephem.Sun(observer)  # ☼
 
-# def sky_position():
-#     current = []
-#     for symbol, body in SOURCE:
-#         if body.alt > 0:
-#             az, alt = [degrees(float(i)) for i in (body.az, body.alt)]
-#             up_or_down =
-#             current.append('{} {:3.0f}°⇔  {:.0f}°{}'.format(
-#                 symbol, az, alt, up_or_down))
-#     yield '   '.join(current)
-#
-#
-# def calculate(symbol, body):
-#     """
-#         Calculate the appropriate rising and setting time. We'll
-#         calculate tomorrow's time if the body is not currently
-#         above the horizon.
-#
-#         TODO - this does not work right if the time is near the rise
-#         or set. Ignoring this for now.
-#     """
-#     if body.alt > 0:
-#         az, alt = [degrees(float(i)) for i in (body.az, body.alt)]
-#         rising_method = observer.previous_rising
-#         srise = 'Rose'
-#     else:
-#         rising_method = observer.next_rising
-#         srise = 'Rise'
-#     fmt = '{} {:%I:%M %p %a} {:.0f}°⇔'
-#     rising = ephem.localtime(rising_method(body))
-#     r = fmt.format(srise, rising, degrees(float(body.az)))
-#     setting = ephem.localtime(observer.next_setting(body))
-#     s = fmt.format('Set', setting, degrees(float(body.az)))
-#     yield '{}{}   {}{}'.format(symbol, r, symbol, s)
-#
-#
-# def sun_and_moon():
-#     "Rise, set and current position of the Sun and Moon"
-#     for symbol, body in SOURCE:
-#         for line in calculate(symbol, body):
-#             yield line
-#
-#
-#
-# def main():
-#     for line in itertools.chain(sky_position(), sun_and_moon(), moon_info()):
-#         print(line)
-#
-# if __name__ == '__main__':
-#     main()
-#
+    if sun.alt > 0:
+        up_or_down = '⬆' if sun.az <= ephem.degrees('180') else '⬇'
+        print('☼ {}{} {} ⇔ '.format(sun.alt, up_or_down, sun.az))
+    elif sun.alt >= ephem.degrees('-6'):
+        print('Civil twilight', sun.alt)
+    elif sun.alt >= ephem.degrees('-12'):
+        print('Nautical twilight', sun.alt)
+    elif sun.alt >= ephem.degrees('-18'):
+        print('Astronomical twilight', sun.alt)
+    else:
+        print("It's dark out there. {}° ⇔".format(sun.az))
+
+    events = make_sun_events(sun, observer, observer.date)
+    for ev in events:
+        print(ev)
+    print(10 * '-')
+    print(events.find_lt(observer.date))
+    print(events.find_gt(observer.date))
