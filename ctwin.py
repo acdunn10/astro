d3880f129aa9938eb5c040e5adf61a672416a749
Mark Sundstrom <mark@mhsundstrom.com>
2013-01-12 15:12:40 -0500

    checkpoint "adp" working

--- ctwin.py
+++ ctwin.py
@@ -1,40 +1,21 @@
+import operator
+import curses
+import itertools
+import collections
 import ephem
 from astro import Comets
 from astro.utils import pairwise
-import operator
+from astro.utils import format_angle as _
+from astro import miles_from_au
 
 PLANETS = (ephem.Mercury, ephem.Venus, ephem.Mars,
            ephem.Jupiter, ephem.Saturn,
            ephem.Uranus, ephem.Neptune)
 STARS = ('Spica', 'Antares', 'Aldebaran', 'Pollux', 'Regulus')
 COMETS = ('C/2012 S1 (ISON)', 'C/2011 L4 (PANSTARRS)')
+MAX_ANGLE = ephem.degrees('20')
 
 
-def make_all_bodies():
-    sun = ephem.Sun()
-    moon = ephem.Moon()
-    planets = [planet() for planet in PLANETS]
-    stars = [ephem.star(name) for name in STARS]
-    comet_dct = Comets()
-    comets = [comet_dct[name] for name in COMETS]
-    return [sun, moon] + planets + comets + stars
-
-
-class Event:
-    def __init__(self, name, field, date, value):
-        self.name = name
-        self.field = field
-        self.date = date
-        self.value = value
-
-    def __str__(self):
-        return "{0.name} {0.field} {0.date} {0.value}".format(self)
-
-        self.events = [
-            Event(name, getattr(body, name), getattr(body, azalt))
-            for name, azalt in (('rise_time', 'rise_az'), ('transit_time', 'transit_alt'), ('set_time', 'set_az'))
-            ]
-        self.events.sort(key=operator.attrgetter('date'))
 
 FIELDS = ('rise_time', 'transit_time', 'set_time')
 AZALT = ('rise_az', 'transit_alt', 'set_az')
@@ -53,9 +34,101 @@ AZALT = ('rise_az', 'transit_alt', 'set_az')
 #         if ev.date >= ephem.now():
 #             print(ev)
 
+class Separation(collections.namedtuple('Separation', 'body1 body2 angle')):
+    def trend(self):
+        "Returns True if the two bodies are getting closer"
+        arrows = "UD" #
+        a = self.body1.copy()
+        b = self.body2.copy()
+        later = ephem.now() + ephem.hour
+        a.compute(later)
+        b.compute(later)
+        return ephem.separation(a, b) < self.angle
+
 class Calculate:
+    def __init__(self, default):
+        self.default = default
+        self.observer = ephem.city('Columbus')
+        self.sun = ephem.Sun()
+        self.moon = ephem.Moon()
+        self.planets = [planet() for planet in PLANETS]
+        self.stars = [ephem.star(name) for name in STARS]
+        comet_dict = Comets()
+        self.comets = [comet_dict[name] for name in COMETS]
+        self.except_stars = [self.sun, self.moon] + \
+                            self.planets + self.comets
+        self.all_bodies = self.except_stars + self.stars
 
+    def update(self, w, default):
+        if default != self.default:
+            w.erase()
+        self.date = ephem.now()
+        self.observer.date = self.date
+        w.addstr(0, 0, "{:%H:%M:%S}".format(
+            ephem.localtime(self.date)))
+        w.addch(0, 30, default)
+        arg = self.observer if default == 'p' else self.date
+        [body.compute(arg) for body in self.all_bodies]
+        if default == 'a':
+            self.update_angles(w)
+        elif default == 'd':
+            self.update_distance(w)
+        elif default == 'p':
+            self.sky_position(w)
+        self.default = default
 
+    def update_angles(self, w):
+        # Calculate angular separation between planetary bodies
+        angles = []
+        for a, b in itertools.combinations(self.all_bodies, 2):
+            angle = ephem.separation(a, b)
+            if angle < MAX_ANGLE:
+                angles.append(Separation(a, b, angle))
+        angles.sort(key=operator.attrgetter('angle'))
+        w.addstr(2, 0, 'Angular separation')
+        for row, sep in enumerate(angles):
+            color = 1 if sep.trend() else 2
+            w.addstr(row + 3, 0,
+                "{1} {0.body1.name} ⇔ {0.body2.name}".format(
+                    sep, _(sep.angle)),
+                curses.color_pair(color))
+            w.clrtoeol()
+
+    def update_distance(self, w):
+        # Calculate distance and velocity for objects of interest
+
+        distances = [
+            Distance(self.date, body)
+            for body in self.except_stars
+        ]
+        distances.sort(key=operator.attrgetter('mph'))
+        w.addstr(2, 0, 'Distances')
+        for row, obj in enumerate(distances):
+            color = 1 if obj.trend else 2
+            w.addstr(row + 3, 0,
+                "{0.mph:7,.0f} mph {0.miles:13,.0f} {0.body.name}".format(obj),
+                curses.color_pair(color))
+
+    def sky_position(self, w):
+        w.addstr(2, 0, 'Azimuth and Altitude')
+        for row, b in enumerate(sorted(self.except_stars, key=operator.attrgetter('alt'), reverse=True)):
+            if b.alt < 0:
+                color = 0
+            else:
+                color = 1 if b.az < ephem.degrees('180') else 2
+            w.addstr(row + 3, 0,
+                "{0.name} {0.az} {0.alt}".format(b),
+                curses.color_pair(color))
+
+class Distance:
+    def __init__(self, date, body):
+        self.body = body
+        self.miles = miles_from_au(body.earth_distance)
+        x = body.copy()
+        x.compute(ephem.date(date + ephem.hour))
+        moved = miles_from_au(x.earth_distance) - self.miles
+        self.trend = moved < 0
+        self.mph = abs(moved)
 
 
 def main(w):
@@ -64,16 +137,23 @@ def main(w):
     curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
     w.timeout(1000)
-    calculate = Calculate()
+    default = 'p'
+    calculate = Calculate(default)
 
     while True:
         try:
-            calculate.update(w)
+            ch = w.getch()
+            if ch == ord('a'):
+                default = 'a'
+            elif ch == ord('d'):
+                default = 'd'
+            elif ch == ord('p'):
+                default = 'p'
+            elif ch == ord('q'):
+                break
+            calculate.update(w, default)
             curses.curs_set(0)
             w.refresh()
-            ch = w.getch()
-            if ch != -1:
-                break
         except KeyboardInterrupt:
             break
 
