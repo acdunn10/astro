ddb761a2354c90db0117e084ccbedc7a08631993
Mark Sundstrom <mark@mhsundstrom.com>
2013-01-16 12:23:30 -0500

    checkpoint before threading

--- watcher.py
+++ watcher.py
@@ -6,7 +6,7 @@ import itertools
 import collections
 import functools
 import logging
-import queue
+import heapq
 import threading
 import time
 import ephem
@@ -22,55 +22,9 @@ STARS = ('Spica', 'Antares', 'Aldebaran', 'Pollux',
 COMETS = ('C/2012 S1 (ISON)', 'C/2011 L4 (PANSTARRS)')
 COMMANDS = 'ademrp'
 
-FIELDS = ('rise_time', 'transit_time', 'set_time')
+FIELDS = ('rise_time', 'transit_time', 'set_time')  # order is important
 AZALT = ('rise_az', 'transit_alt', 'set_az')
 
-
-# class Calculate:
-#     "Manage the display of data in the curses window"
-#     def __init__(self):
-#         "Load up initial data, including the comets"
-#         self.cmd = 'p'
-#         self.observer = ephem.city(CITY)
-#         self.sun = ephem.Sun()
-#         self.moon = ephem.Moon()
-#         self.planets = [planet() for planet in PLANETS]
-#         self.stars = [ephem.star(name) for name in STARS]
-#         comet_dict = Comets()
-#         logger.info("Comets last-modified: {}".format(comet_dict.last_modified))
-#         self.comets = [comet_dict[name] for name in COMETS]
-#         self.except_stars = [self.sun, self.moon] + \
-#                             self.planets + self.comets
-#         self.all_bodies = self.except_stars + self.stars
-#         self.rs = collections.defaultdict(list)
-#         self.rs_events = []
-#
-#     def compute(self):
-#         "Compute data for all bodies, not using the Observer"
-#         [body.compute(self.date) for body in self.all_bodies]
-#         return self
-#
-#     def compute_observer(self):
-#         "Compute with the Observer"
-#         [body.compute(self.observer) for body in self.all_bodies]
-#         return self
-#
-#     def calc_rise_set(self, base_date):
-#         "Get rise, transit and setting info for the specified date"
-#         self.observer.date = ephem.Date(base_date)
-#         logger.info("calc_rise_set: {0.observer.date}".format(self))
-#         for body in self.except_stars:
-#             body.compute(self.observer)
-#             for fieldname, azalt in zip(FIELDS, AZALT):
-#                 date = getattr(body, fieldname)
-#                 if date is None:
-#                     continue
-#                 self.rs[base_date].append({
-#                                'key': fieldname.split('_')[0],
-#                                'name': body.name,
-#                                'date': ephem.localtime(date),
-#                                'azalt': getattr(body, azalt),
-#                                'symbol': get_symbol(body)})
 
 @functools.total_ordering
 class Event:
@@ -90,40 +44,46 @@ class Event:
     def __lt__(self, other):
         return self.priority < other.priority
 
+def reschedule_event(ev, events, bodies, observer):
+    if ev.event_name == 'rise_time':
+        body = bodies[ev.body_name]
+        date = observer.next_rising(body, start=ev.date)
+        if date is not None:
+            new_ev = Event(ev.body_name, date, 'rise_time', body.rise_az)
+            heapq.heappush(events, new_ev)
+            print("Rescheduled: {}".format(new_ev))
 
 def rise_set_worker():
     print("rise_set_worker startup")
-    bodies = [ephem.Sun(), ephem.Moon()] +\
+    initial_bodies = [ephem.Sun(), ephem.Moon()] +\
              [planet() for planet in PLANETS] +\
              [ephem.star(name) for name in stars.keys()]
-    print(len(bodies),'bodies')
     observer = ephem.city(CITY)
-    for body in bodies:
-        body.compute(observer)
-        #print(body.name, body.ra, body.rise_time)
-    events = queue.PriorityQueue()
-    for body in bodies:
+    [body.compute(observer) for body in initial_bodies]
+    bodies = {body.name: body for body in initial_bodies}
+    events = []
+    for body in bodies.values():
         for fieldname, azalt in zip(FIELDS, AZALT):
             date = getattr(body, fieldname)
             azalt = getattr(body, azalt)
-            ev = Event(body.name, date, fieldname, azalt)
-            events.put(ev)
+            heapq.heappush(events, Event(body.name, date, fieldname, azalt))
     while True:
         now = ephem.now()
-        print("Queue {:4d} {}".format(events.qsize(), now))
+        print("Queue {:4d} {}".format(len(events), now))
         try:
-            ev = events.get()
+            ev = heapq.heappop(events)
             if ev.date is None:
                 print("Skipping event with no date:", ev)
             elif ev.date < now:
                 print("Event has already occurred:", ev)
+                reschedule_event(ev, events, bodies, observer)
             else:
                 print("Waiting for next event:", ev)
                 time_to_go = 86400 * (ev.date - now)
                 print("time to go:", time_to_go)
                 time.sleep(time_to_go)
                 print("done sleeping")
-        except queue.Empty:
+        except IndexError:
             break
 
 def main():
