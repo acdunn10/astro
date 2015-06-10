2ad31d79cffd063d23ba3433217597c1e76c16dd
Mark Sundstrom <mark@mhsundstrom.com>
2015-06-10 07:17:26 -0400

    Now version 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'Astro',
-    version = 0.1,
+    version = 0.2,
     description = 'Experiments with astronomical calculations',
     author = 'Mark Sundstrom',
     author_email = 'mark@mhsundstrom.com',
